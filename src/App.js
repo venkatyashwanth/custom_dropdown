b6508fc3df90be4539bffab1da801c0b934ac70a
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-10-04 14:32:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Dropdown } from './components/Dropdown';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+        <Dropdown/>
+    </div>
+  );
+}
+
+export default App;
